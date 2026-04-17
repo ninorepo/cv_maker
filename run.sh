8d98b2f307e682ecc73e13d35d5635f0355fd3e2
Ninoyu <136919890+ninorepo@users.noreply.github.com>
2026-04-17 21:53:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-#./clear.sh
+./clear.sh
 ./init_templates.sh
 ./init_watermark.sh
 ./compile_templates.sh
 ./init_attachments.sh
 ./burn_watermarks.sh
-#./merge_pdfs.sh
-#./clear.sh
+./merge_pdfs.sh
+./clear.sh
